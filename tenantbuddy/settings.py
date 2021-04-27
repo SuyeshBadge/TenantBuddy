2cd5d1a213b7cc8396eacbd17f796583bbe9eeae
SuyeshBadge <suyeshbadge@gmail.com>
2021-04-27 15:41:24 +0530

    aws key regenerated

--- tenantbuddy/settings.py
+++ tenantbuddy/settings.py
@@ -150,8 +150,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Aws S3 Configuration
-AWS_ACCESS_KEY_ID = 'AKIAR6UF5G3MKIEWVGVF'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'tenantbuddy'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
